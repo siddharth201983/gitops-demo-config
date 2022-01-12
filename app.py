6cc5530a85be2cffa15ec9aa4d543c46f882a732
vikram <vikram@gmail.com>
2022-01-12 08:44:41 +0530

    init

--- app.py
+++ app.py
@@ -30,7 +30,11 @@ def health():
 
 @app.route("/")
 def home():
+<<<<<<< HEAD
     return "Hello World! FROM MAIN BRANCH"
+=======
+    return "Hello World!"
+>>>>>>> 8e5eaba (init)
    
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("5000"), debug=True)
